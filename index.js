338e6f01708ef697ff9900bc3d176a5be3ea1078
yuanjunliang <1610712512@qq.com>
2018-11-24 01:17:11 +0800

    update to new

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const BTPack = require('./lib/BTPack')
 const BTUnpack = require('./lib/BTUnpack')
-const BasicPack = require('./lib/BasicPack')
-
 
 module.exports = {
     BTPack,
-    BasicPack,
     BTUnpack
 }
